47ffc40d74f6aebab0854cbc852906a4ed0afa5f
Diaa Qishtaa <darness.alex@gmail.com>
2026-07-07 15:12:15 +0300

    Track Android project

---
+++ android/app/.cxx/RelWithDebInfo/6h6v2g50/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/hermes-engine/hermes-engineConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET hermes-engine::libhermes)
+add_library(hermes-engine::libhermes SHARED IMPORTED)
+set_target_properties(hermes-engine::libhermes PROPERTIES
+    IMPORTED_LOCATION "C:/Users/Darness/.gradle/caches/8.14.3/transforms/e0dd1161cdfd6f674c9b0917c0398251/transformed/hermes-android-0.81.5-release/prefab/modules/libhermes/libs/android.armeabi-v7a/libhermes.so"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/Darness/.gradle/caches/8.14.3/transforms/e0dd1161cdfd6f674c9b0917c0398251/transformed/hermes-android-0.81.5-release/prefab/modules/libhermes/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
